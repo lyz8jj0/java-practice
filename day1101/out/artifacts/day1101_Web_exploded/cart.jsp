48349fbe17b52ecec27fd3026d25282350216bfe
lyz8jj0 <844216765@qq.com>
2018-08-01 19:04:23 +0800

    添加到购物车-代码实现

---
+++ day1101/out/artifacts/day1101_Web_exploded/cart.jsp
@@ -0,0 +1,47 @@
+<%@ page import="java.util.Map" %><%--
+  Created by IntelliJ IDEA.
+  User: Administrator
+  Date: 2018/8/1
+  Time: 18:40
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>购物车</title>
+</head>
+<body>
+<table border="1" align="center" width="">
+    <tr>
+        <td>商品名称</td>
+        <td>商品数量</td>
+    </tr>
+    <%
+        //1，获取购物车
+        Map<String, Integer> map = (Map<String, Integer>) session.getAttribute("cart");
+
+        //2，判断购物车是否为空
+        if (map == null) {
+            //2.1若不为空：亲，购物车空空，先去逛逛~~
+            out.print("<tr><td colspan='2'>亲，购物车空空，先去逛逛~~~</td></tr>");
+        } else {
+            //2.2若不为空：遍历购物车
+            for (String name : map.keySet()) {
+                out.print("<tr>");
+                out.print("<td>");
+                out.print(name);
+                out.print("</td>");
+                out.print("<td>");
+                out.print(map.get(name));
+                out.print("</td>");
+                out.print("</tr>");
+            }
+        }
+    %>
+</table>
+<hr>
+<center>
+    <a href="/day1101/product_list.jsp">继续购物</a>&nbsp;&nbsp;&nbsp;
+</center>
+</body>
+</html>
---
+++ day1101/src/com/itheima/web/servlet/Add2CartServlet.java
@@ -0,0 +1,66 @@
+package com.itheima.web.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 添加到购物车
+ */
+@WebServlet(name = "Add2CartServlet", urlPatterns = {"/add2Cart"})
+public class Add2CartServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @SuppressWarnings("unchecked")
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //0，设置编码
+        response.setContentType("text/html;charset=utf-8");
+        PrintWriter w = response.getWriter();
+
+        //1，获取商品的名称
+        String name = request.getParameter("name");
+//        name = new String(name.getBytes("ISO8859-1"), "UTF-8");
+
+        //2，将商品添加到购物车
+        //2.1，从session中获取购物车
+        Map<String, Integer> map = (Map<String, Integer>) request.getSession().getAttribute("cart");
+
+        Integer count = null;
+
+        //2.2，判断购物车是否为空
+        if (map == null) {
+            //第一次购物 创建购物车
+            map = new HashMap<>();
+
+            //将购物车放入session中
+            request.getSession().setAttribute("cart", map);
+
+            count = 1;
+        } else {
+            //购物车不为空 继续判断购物车中是否有该商品
+            count = map.get(name);
+            if (count == null) {
+                //购物车中没有该商品
+                count = 1;
+            } else {
+                count++;
+            }
+
+        }
+        //将商品放入购物车中
+        map.put(name, count);
+
+        //3，提示信息
+        w.print("已经将<b>" + name + "</b>添加到购物车中<hr>");
+        w.print("<a href='" + request.getContextPath() + "/product_list.jsp'>继续购物</a>&nbsp;&nbsp;&nbsp;");
+        w.print("<a href='" + request.getContextPath() + "/cart.jsp'>查看购物车</a>");
+    }
+}
