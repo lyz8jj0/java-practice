b633f75498b3504c9bc7625d191e590b8bd08080
lyz8jj0 <844216765@qq.com>
2018-08-15 18:12:40 +0800

    装饰者模式

--- day07/src/com/itheima/a_jdbc/a_hello/Hello.java
+++ day07/src/com/itheima/a_jdbc/a_hello/Hello.java
@@ -3,10 +3,7 @@ package com.itheima.a_jdbc.a_hello;
 import com.itheima.utils.JdbcUtils;
 import org.junit.Test;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+import java.sql.*;
 
 public class Hello {
     @Test
@@ -81,5 +78,56 @@ public class Hello {
         }
     }
 
+    //更新
+    @Test
+    public void f11() {
+        Connection conn = null;
+        PreparedStatement st = null;
+        ResultSet rs = null;
+
+        try {
+            conn = JdbcUtils.getConnection();
+            String sql = "update category set cname = ? where cid = ?";
+            st = conn.prepareStatement(sql);
+            st.setString(1, "手机");
+            st.setString(2, "c006");
+            int i = st.executeUpdate();
+            if (i == 1) {
+                System.out.println("success");
+            } else {
+                System.out.println("error");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            //释放资源
+            JdbcUtils.closeResource(conn, st, rs);
+        }
+    }
+
+    //删除
+    @Test
+    public void f12() {
+        Connection conn = null;
+        PreparedStatement st = null;
+        ResultSet rs = null;
+
+        try {
+            conn = JdbcUtils.getConnection();
+            String sql = "delete from category where cid = ?";
+            st = conn.prepareStatement(sql);
+            st.setString(1, "c006");
+            int i = st.executeUpdate();
+            if (i == 1) {
+                System.out.println("success");
+            } else {
+                System.out.println("error");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            JdbcUtils.closeResource(conn, st, rs);
+        }
+    }
 }
 
---
+++ day07/src/com/itheima/b_datasourc/c_warp/Car.java
@@ -0,0 +1,7 @@
+package com.itheima.b_datasourc.c_warp;
+
+public interface Car {
+    void run();
+
+    void stop();
+}
