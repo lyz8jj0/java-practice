67a9326d3bac9b0ac7a975fe94072cde13e10298
lyz8jj0 <844216765@qq.com>
2018-09-03 10:31:42 +0800

    编写myTest案例-java中的三个注解

--- day16/src/com/itheima/b_filter/i_encoding/LoginServlet.java
+++ day16/src/com/itheima/b_filter/i_encoding/LoginServlet.java
@@ -7,6 +7,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+/**
+ * @author Administrator
+ */
 @WebServlet(name = "LoginServlet", urlPatterns = {"/login"})
 public class LoginServlet extends HttpServlet {
     @Override
---
+++ day17/src/com/itheima/domain/User.java
@@ -0,0 +1,43 @@
+package com.itheima.domain;
+
+import java.util.Date;
+import java.util.List;
+
+public class User {
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+    @SuppressWarnings("all")
+    private String username;
+
+    @SuppressWarnings("all")
+    private List list;
+
+    @SuppressWarnings("all")
+    public static void main(String[] args) {
+        new Date().toLocaleString();
+        add();
+    }
+
+
+    @Deprecated
+    private static void add() {
+
+    }
+
+    public static void add(int... args) {
+
+    }
+}
