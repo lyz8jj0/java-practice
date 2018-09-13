c8374a19113ebe6981b891e23deede412faae2a6
lyz8jj0 <844216765@qq.com>
2018-09-13 18:51:32 +0800

    工厂模式

---
+++ store/src/com/itheima/utils/BeanFactory.java
@@ -0,0 +1,20 @@
+package com.itheima.utils;
+
+import org.dom4j.Document;
+
+/**
+ * 实体工厂类
+ */
+public class BeanFactory {
+    public static Object getBean(String id) {
+        //通过id获取一个指定的实现类
+
+        //1,获取document对象
+        Document doc = new 
+        //2,获取指定的bean对象
+
+        //3,获取bean对象的class属性
+
+        //反射
+    }
+}
