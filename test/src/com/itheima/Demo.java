6d88a0ccc4f9393fc28e9b626b558a4421235b09
lyz8jj0 <844216765@qq.com>
2018-10-12 18:12:44 +0800

    试一下idea上使用git2

---
+++ test/src/com/itheima/Demo.java
@@ -0,0 +1,33 @@
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.*;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.charset.Charset;
+
+public class Demo {
+    private static String readAll(Reader rd) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        int cp;
+        while ((cp = rd.read()) != -1) {
+            sb.append((char) cp);
+        }
+        return sb.toString();
+    }
+
+    private static JSONObject getRequestFromUrl(String url) throws IOException, JSONException {
+        URL realUrl = new URL(url);
+        URLConnection conn = realUrl.openConnection();
+        InputStream instream = conn.getInputStream();
+        try {
+            BufferedReader rd = new BufferedReader(new InputStreamReader(instream, Charset.forName("UTF-8")));
+            String jsonText = readAll(rd);
+            JSONObject json = new JSONObject(jsonText);
+            return json;
+        } finally {
+            instream.close();
+        }
+    }
+
+}
---
+++ test/src/com/itheima/GitTest.java
@@ -0,0 +1,7 @@
+package com.itheima;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("idea上使用git");
+    }
+}
